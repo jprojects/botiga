27ee1b1cbf3b4df6a17d899e6f71d44d6345bd7c
jprojects <kim@aficat.com>
2017-08-04 11:01:20 +0200

    install sql fixes

--- packages/administrator/sql/install.mysql.utf8.sql
+++ packages/administrator/sql/install.mysql.utf8.sql
@@ -13,7 +13,7 @@
 --
 
 CREATE TABLE IF NOT EXISTS `#__botiga_brands` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(150) NOT NULL DEFAULT '',
   `published` tinyint(1) NOT NULL DEFAULT '0',
   `language` char(7) NOT NULL DEFAULT '0',
@@ -21,7 +21,8 @@ CREATE TABLE IF NOT EXISTS `#__botiga_brands` (
   `id_familia` int(11) DEFAULT NULL,
   `id_subfamilia` int(11) DEFAULT NULL,
   `factusol_codfte` int(11) DEFAULT '0',
-  `ordering` int(11) NOT NULL
+  `ordering` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -31,12 +32,13 @@ CREATE TABLE IF NOT EXISTS `#__botiga_brands` (
 --
 
 CREATE TABLE IF NOT EXISTS `#__botiga_comandes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `data` datetime NOT NULL,
   `userid` int(11) NOT NULL,
   `status` smallint(1) NOT NULL DEFAULT '0',
   `published` tinyint(1) NOT NULL DEFAULT '0',
-  `language` char(7) NOT NULL DEFAULT '0'
+  `language` char(7) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -46,12 +48,13 @@ CREATE TABLE IF NOT EXISTS `#__botiga_comandes` (
 --
 
 CREATE TABLE IF NOT EXISTS `#__botiga_comandesDetall` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `idComanda` int(11) NOT NULL,
   `idItem` int(11) NOT NULL,
   `price` float(10,2) NOT NULL DEFAULT '0.00',
   `published` tinyint(1) NOT NULL DEFAULT '0',
-  `language` char(7) NOT NULL DEFAULT '0'
+  `language` char(7) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -63,7 +66,7 @@ CREATE TABLE IF NOT EXISTS `#__botiga_comandesDetall` (
 --
 
 CREATE TABLE IF NOT EXISTS `#__botiga_rebuts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `data` datetime NOT NULL,
   `userid` int(11) NOT NULL DEFAULT '0',
   `import` float(10,2) NOT NULL DEFAULT '0.00',
@@ -74,7 +77,8 @@ CREATE TABLE IF NOT EXISTS `#__botiga_rebuts` (
   `payment_status` varchar(10) NOT NULL DEFAULT '',
   `titular` varchar(100) NOT NULL DEFAULT '',
   `iban` varchar(50) NOT NULL DEFAULT '',
-  `paypal` varchar(50) NOT NULL DEFAULT ''
+  `paypal` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
@@ -82,7 +86,7 @@ CREATE TABLE IF NOT EXISTS `#__botiga_rebuts` (
 --
 
 CREATE TABLE IF NOT EXISTS `#__botiga_items` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `catid` int(11) NOT NULL DEFAULT '0',
   `name` varchar(150) NOT NULL DEFAULT '',
   `brand` int(11) NOT NULL DEFAULT '0',
@@ -99,7 +103,8 @@ CREATE TABLE IF NOT EXISTS `#__botiga_items` (
   `language` char(7) NOT NULL DEFAULT '0',
   `ref` varchar(15) DEFAULT NULL,
   `factusol_codart` varchar(13) DEFAULT NULL,
-  `sincronitzat` int(11) DEFAULT '0'
+  `sincronitzat` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -109,9 +114,10 @@ CREATE TABLE IF NOT EXISTS `#__botiga_items` (
 --
 
 CREATE TABLE `#__botiga_coupons` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `title` varchar(50) NOT NULL,
-  `coupon` varchar(50) NOT NULL
+  `coupon` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -121,7 +127,7 @@ CREATE TABLE `#__botiga_coupons` (
 --
 
 CREATE TABLE `#__botiga_users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `usergroup` int(11) DEFAULT NULL,
   `nom_empresa` varchar(255) DEFAULT NULL,
   `mail_empresa` varchar(255) DEFAULT NULL,
@@ -135,7 +141,8 @@ CREATE TABLE `#__botiga_users` (
   `telefon` varchar(50) DEFAULT NULL,
   `activitat` varchar(255) DEFAULT NULL,
   `nivell` int(11) NOT NULL DEFAULT '1',
-  `published` tinyint(1) NOT NULL DEFAULT '0'
+  `published` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
