7d9544a39a795e4f24045f16aa11dcaecb273d70
kim <kim@aficat.com>
2019-05-02 17:46:46 +0200

    plugin sync users

---
+++ packages/plugins/botiga_users/botiga_users.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+/**
+ * Joomla User plugin
+ *
+ * @package		Joomla.Plugin
+ * @subpackage	User.joomla
+ * @since		1.5
+ */
+class plgUserBotiga_users extends JPlugin
+{
+	/**
+	 * Remove all sessions for the user name
+	 *
+	 * Method is called after user data is deleted from the database
+	 *
+	 * @param	array		$user	Holds the user data
+	 * @param	boolean		$succes	True if user was succesfully stored in the database
+	 * @param	string		$msg	Message
+	 *
+	 * @return	boolean
+	 * @since	1.6
+	 */
+	public function onUserAfterDelete($user, $succes, $msg)
+	{
+		if (!$succes) {
+			return false;
+		}
+
+		$db = JFactory::getDbo();
+		$db->setQuery(
+			'DELETE FROM '.$db->quoteName('#__botiga_users') .
+			' WHERE '.$db->quoteName('userid').' = '.(int) $user['id']
+		);
+		$db->Query();
+
+		return true;
+	}
+
+	/**
+	 * Utility method to act on a user after it has been saved.
+	 *
+	 * This method sends a registration email to new users created in the backend.
+	 *
+	 * @param	array		$user		Holds the new user data.
+	 * @param	boolean		$isnew		True if a new user is stored.
+	 * @param	boolean		$success	True if user was succesfully stored in the database.
+	 * @param	string		$msg		Message.
+	 *
+	 * @return	void
+	 * @since	1.6
+	 */
+	public function onUserAfterSave($user, $isnew, $success, $msg)
+	{
+		// Initialise variables.
+		$app	= JFactory::getApplication();
+		$db 	= JFactory::getDbo();
+
+		if ($isnew) {
+			// TODO: Suck in the frontend registration emails here as well. Job for a rainy day.
+
+			if ($app->isAdmin()) {
+				return;
+			}
+			
+			$bio = new stdClass();
+			$bio->userid = $user['id'];
+			$bio->nom_empresa   = $user['name'];
+			$bio->mail_empresa  = $user['email'];
+			$bio->published     = 0;
+			$bio->usergroup     = 2;
+			$db->insertObject('#__botiga_users', $bio); 
+		}
+		else {
+			return;
+		}
+	}
+
+	/**
+	 * This method will return a user object
+	 *
+	 * If options['autoregister'] is true, if the user doesn't exist yet he will be created
+	 *
+	 * @param	array	$user		Holds the user data.
+	 * @param	array	$options	Array holding options (remember, autoregister, group).
+	 *
+	 * @return	object	A JUser object
+	 * @since	1.5
+	 */
+	protected function _getUser($user, $options = array())
+	{
+		$instance = JUser::getInstance();
+		if ($id = intval(JUserHelper::getUserId($user['username'])))  {
+			$instance->load($id);
+			return $instance;
+		}
+
+		//TODO : move this out of the plugin
+		jimport('joomla.application.component.helper');
+		$config	= JComponentHelper::getParams('com_users');
+		// Default to Registered.
+		$defaultUserGroup = $config->get('new_usertype', 2);
+
+		$acl = JFactory::getACL();
+
+		$instance->set('id'			, 0);
+		$instance->set('name'			, $user['fullname']);
+		$instance->set('username'		, $user['username']);
+		$instance->set('password_clear'	, $user['password_clear']);
+		$instance->set('email'			, $user['email']);	// Result should contain an email (check)
+		$instance->set('usertype'		, 'deprecated');
+		$instance->set('groups'		, array($defaultUserGroup));
+
+		//If autoregister is set let's register the user
+		$autoregister = isset($options['autoregister']) ? $options['autoregister'] :  $this->params->get('autoregister', 1);
+
+		if ($autoregister) {
+			if (!$instance->save()) {
+				return JError::raiseWarning('SOME_ERROR_CODE', $instance->getError());
+			}
+		}
+		else {
+			// No existing user and autoregister off, this is a temporary user.
+			$instance->set('tmp_user', true);
+		}
+
+		return $instance;
+	}
+}
